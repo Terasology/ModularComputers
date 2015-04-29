1cfe4880e115a819b5e4e40e6a42a875cda81f90
MarcinSc <marcins78@gmail.com>
2015-04-29 09:41:06 -0700

    Added documentation for the Internal Storage module methods.

--- src/main/java/org/terasology/computer/module/storage/StorageModuleCommonSystem.java
+++ src/main/java/org/terasology/computer/module/storage/StorageModuleCommonSystem.java
@@ -15,6 +15,7 @@
  */
 package org.terasology.computer.module.storage;
 
+import org.terasology.browser.data.ParagraphData;
 import org.terasology.computer.module.inventory.InventoryModuleCommonSystem;
 import org.terasology.computer.system.common.ComputerModuleRegistry;
 import org.terasology.computer.ui.documentation.DocumentationBuilder;
@@ -23,6 +24,7 @@ import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.registry.In;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.TreeMap;
 
@@ -56,6 +58,45 @@ public class StorageModuleCommonSystem extends BaseComponentSystem {
                 new HashMap<String, String>() {{
                     put("getInputInventoryBinding", "[Inventory Binding] Returns inventory binding allowing to put items into this Internal storage.");
                     put("getOutputInventoryBinding", "[Inventory Binding] Returns inventory binding allowing to extract items from this Internal storage.");
-                }}, null);
+                }},
+                new HashMap<String, Collection<ParagraphData>>() {{
+                    put("getInputInventoryBinding", DocumentationBuilder.createExampleParagraphs(
+                            "This example creates input and output inventory binding for the internal storage and " +
+                                    "an input and output inventory binding for storage above the computer, then dumps " +
+                                    "the contents of the inventory above into the internal storage, waits for 5 seconds " +
+                                    "and dumps the items back into the inventory above.  Please make sure " +
+                                    "this computer has a module of Inventory Manipulator type in any of its slots, as well " +
+                                    "as Internal Storage module.",
+                            "var storageMod = computer.bindModuleOfType(\"" + COMPUTER_STORAGE_MODULE_TYPE + "\");\n" +
+                                    "var inventoryMod = computer.bindModuleOfType(\"" + InventoryModuleCommonSystem.COMPUTER_INVENTORY_MODULE_TYPE + "\");\n" +
+                                    "var inputInternal = storageMod.getInputInventoryBinding();\n" +
+                                    "var outputInternal = storageMod.getOutputInventoryBinding();\n" +
+                                    "var inputUp = inventoryMod.getInputInventoryBinding(\"up\");\n" +
+                                    "var outputUp = inventoryMod.getOutputInventoryBinding(\"up\");\n" +
+                                    "\n" +
+                                    "inventoryMod.dump(outputUp, inputInternal);\n" +
+                                    "os.waitFor(os.createSleepMs(5000));\n" +
+                                    "inventoryMod.dump(outputInternal, inputUp);"
+                    ));
+                    put("getOutputInventoryBinding", DocumentationBuilder.createExampleParagraphs(
+                            "This example creates input and output inventory binding for the internal storage and " +
+                                    "an input and output inventory binding for storage above the computer, then dumps " +
+                                    "the contents of the inventory above into the internal storage, waits for 5 seconds " +
+                                    "and dumps the items back into the inventory above.  Please make sure " +
+                                    "this computer has a module of Inventory Manipulator type in any of its slots, as well " +
+                                    "as Internal Storage module.",
+                            "var storageMod = computer.bindModuleOfType(\"" + COMPUTER_STORAGE_MODULE_TYPE + "\");\n" +
+                                    "var inventoryMod = computer.bindModuleOfType(\"" + InventoryModuleCommonSystem.COMPUTER_INVENTORY_MODULE_TYPE + "\");\n" +
+                                    "var inputInternal = storageMod.getInputInventoryBinding();\n" +
+                                    "var outputInternal = storageMod.getOutputInventoryBinding();\n" +
+                                    "var inputUp = inventoryMod.getInputInventoryBinding(\"up\");\n" +
+                                    "var outputUp = inventoryMod.getOutputInventoryBinding(\"up\");\n" +
+                                    "\n" +
+                                    "inventoryMod.dump(outputUp, inputInternal);\n" +
+                                    "os.waitFor(os.createSleepMs(5000));\n" +
+                                    "inventoryMod.dump(outputInternal, inputUp);"
+                    ));
+                }}
+        );
     }
 }
