bcc3c9d6ce64b1d22ca5578ff551bfc82c2c02cd
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:15 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/computer/component/ComputerComponent.java
+++ src/main/java/org/terasology/computer/component/ComputerComponent.java
@@ -1,31 +1,19 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.computer.component;
 
-import org.terasology.engine.entitySystem.Component;
+import com.google.common.collect.Maps;
 import org.terasology.engine.network.Replicate;
 import org.terasology.engine.world.block.ForceBlockActive;
 import org.terasology.engine.world.block.items.AddToBlockBasedItem;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ForceBlockActive
 @AddToBlockBasedItem
-public class ComputerComponent implements Component {
+public class ComputerComponent implements Component<ComputerComponent> {
     public Map<String, String> programs = new HashMap<>();
     public int moduleSlotStart;
     public int moduleSlotCount = 4;
@@ -34,4 +22,15 @@ public class ComputerComponent implements Component {
     public int memorySize = 1024;
     @Replicate
     public int computerId = -1;
+
+    @Override
+    public void copy(ComputerComponent other) {
+        this.programs = Maps.newHashMap(other.programs);
+        this.moduleSlotStart = other.moduleSlotStart;
+        this.moduleSlotCount = other.moduleSlotCount;
+        this.cpuSpeed = other.cpuSpeed;
+        this.stackSize = other.stackSize;
+        this.memorySize = other.memorySize;
+        this.computerId = other.computerId;
+    }
 }
